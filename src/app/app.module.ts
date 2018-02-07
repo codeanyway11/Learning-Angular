72a3b832bfec64f7560cf2eae2fbeb03b4c68ab8
Charu Agarwal <c_agarwal@ymail.com>
2018-02-07 19:00:23 +0530

    ADD A component as a class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { FirstServerComponent } from './servers/first-server/first-server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    FirstServerComponent
   ],
   imports: [
     BrowserModule,
