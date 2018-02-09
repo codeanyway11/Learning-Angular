736c38e4c9dd02bddd549255be540c9ecd33f667
Charu Agarwal <c_agarwal@ymail.com>
2018-02-09 18:49:28 +0530

    Cleaning up to get the project started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
   ],
   imports: [
     BrowserModule,
@@ -17,6 +15,5 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   providers: [],
   bootstrap: [AppComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-
 })
 export class AppModule { }
